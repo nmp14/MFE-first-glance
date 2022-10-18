705ee08f9a5322eca32d0b0e52062e1fb75ccecc
Nathan Peek <nmp14fsu@gmail.com>
2022-10-17 22:32:11 -0400

    Add moduleFedereation

--- mfe-1/webpack.config.js
+++ mfe-1/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 
 module.exports = {
   mode: "development",
@@ -9,6 +10,13 @@ module.exports = {
   },
 
   plugins: [
+    new ModuleFederationPlugin({
+      name: 'mfe1',
+      filename:'remoteEntry.js',
+      exposes: {
+        './mfe1Index': './src/index',
+      },
+    }),
     new HtmlWebpackPlugin({
       template: './public/index.html'
     })
